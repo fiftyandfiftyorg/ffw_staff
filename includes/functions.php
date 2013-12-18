aa2f922514f6546fa82ba012542dcf06a1ea0fa5
bryanmonzon <monzonb@gmail.com>
2013-12-18 15:39:44 -0800

    new function to see if diable link is set

--- includes/functions.php
+++ includes/functions.php
@@ -5,3 +5,13 @@
 
 // Exit if accessed directly
 if ( ! defined( 'ABSPATH' ) ) exit;
+
+function ffw_staff_disable_link()
+{
+    global $ffw_staff_settings;
+
+   $ffw_staff_disable = isset( $ffw_staff_settings['staff_disable_link_to_single'] ) ? true : false;
+
+   return $ffw_staff_disable;
+    
+}
